4d31e63a91a22492ab721af86470901e2b35689d
0xcoded <130364027+0xcoded@users.noreply.github.com>
2024-09-14 13:36:24 +0200

    Create tester.py

---
+++ tester.py
@@ -0,0 +1,20 @@
+import socket
+
+IP = input("IP: ")
+
+Port = input("Puerto (25565): ")
+if Port != "":
+        Port = int(Port)
+else:
+        Port = 25565
+
+try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(0.5)
+                #s.connect(("na18.holy.gg", 26247))
+                #s.connect((str(IP), 26247))
+                s.connect((str(IP), Port))
+
+                print("El servidor está ABIERTO(" + IP + ":" + str(Port) + ")")
+except:
+        print("Servidor NO DISPONIBLE (" + IP + ":" + str(Port) + ")")
